77be7767fa53d10bdddc8940e8e26b55a1df81a0
Ahmed Olumide Julius <olumideahmed2017@gmail.com>
2026-07-14 16:29:57 +0100

    Create Day3c.py

---
+++ Day3c.py
@@ -0,0 +1,15 @@
+#To grade Student mark
+score = int(input("Enter the score: "))
+
+if score >= 70:
+    print("Grade A")
+    
+elif score >= 60:
+    print("Grade B")
+
+elif score >= 50:
+    print("Grade C")
+elif score >= 40:
+    print("Grade D")
+else:
+    print("you fail")
