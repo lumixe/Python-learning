01fa15f3ddac29291811401955840b508524a2c4
Ahmed Olumide Julius <olumideahmed2017@gmail.com>
2026-07-14 14:46:22 +0100

    Create Day2.py

---
+++ Day2.py
@@ -0,0 +1,5 @@
+name = input("what is your name?: ")
+fav_club = input("what is your favourite club?: ")
+fav_pro_lang = input("what is your favourite programming language?: ")
+
+print(f"{name} supports {fav_club} and loves {fav_pro_lang}.")
